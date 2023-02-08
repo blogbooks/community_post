aeb05bdaecd5a88b8b3f945b8c51b5006cf3f190
harun#9676 / BlogBooks <77278342+Nobody-Harun@users.noreply.github.com>
2023-02-08 14:03:54 +0900

    Create articlesMaps.js

---
+++ articlesMaps.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const fs = require('fs');
+
+// ファイルタイプの列挙体（のつもり）
+const FileType = {
+  File: 'file',
+  Directory: 'directory',
+  Unknown: 'unknown'
+}
+
+/**
+ * ファイルの種類を取得する
+ * @param {string} path パス
+ * @return {FileType} ファイルの種類
+ */
+const getFileType = path => {
+  try {
+    const stat = fs.statSync(path);
+
+    switch (true) {
+      case stat.isFile():
+        return FileType.File;
+
+      case stat.isDirectory():
+        return FileType.Directory;
+
+      default:
+        return FileType.Unknown;
+    }
+
+  } catch(e) {
+    return FileType.Unknown;
+  }
+}
+
+/**
+ * 指定したディレクトリ配下のすべてのファイルをリストアップする
+ * @param {string} dirPath 検索するディレクトリのパス
+ * @return {Array<string>} ファイルのパスのリスト
+ */
+const listFiles = dirPath => {
+  const ret = [];
+  const paths = fs.readdirSync(dirPath);
+
+  paths.forEach(a => {
+    const path = `${dirPath}/${a}`;
+
+    switch (getFileType(path)) {
+      case FileType.File:
+        if (!path.endswith(".md")) break;
+        ret.push(path);
+        break;
+
+      case FileType.Directory:
+        ret.push(...listFiles(path));
+        break;
+
+      default:
+        /* noop */
+    }
+  })
+
+  return ret;
+};
+
+const dirPath = path.resolve(__dirname, 'target');
+const list = listFiles(dirPath);
+let mapData = [];
+
+list.forEach(d => {
+    const markdown = fs.readFileSync(d);
+    if (markdown.match(/^---[\s\S]*---/) == null) {
+        
+    } else {
+        const meta = yaml.load(markdown.match(/^---[\s\S]*?---/)[0].substring(3, markdown.match(/^---[\s\S]*?---/)[0].length - 3));
+        mapData.push({"path": d, "title": meta.title, "timestamp": meta.timestamp, "description": meta.description});
+    }
+})
+
+fs.writeFileSync("../.contents", mapData);
